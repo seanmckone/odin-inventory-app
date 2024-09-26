6fb62430dbce4e3154f928474d86ade4dae2cb9b
SDMckone <seandmckone@gmail.com>
2024-09-26 16:12:55 -0400

    Make "/dashboard" the index route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,7 +4,12 @@ const indexRouter = Router();
 const indexController = require("../controllers/indexController");
 
 // Routing
-indexRouter.get("/", indexController.getItems);
+indexRouter.get("/dashboard", indexController.getItems);
+
+// Redirect "/" route to "/dashboard"
+indexRouter.get("/", (req, res) => {
+  res.redirect("/dashboard");
+});
 
 // Export router
 module.exports = indexRouter;
