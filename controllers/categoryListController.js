8e13632485f97525daad2fc33ae64f491ec0d7b1
SDMckone <seandmckone@gmail.com>
2024-12-15 18:15:12 -0500

    Fix getSubCategories and implement subcat page

--- controllers/categoryListController.js
+++ controllers/categoryListController.js
@@ -27,35 +27,49 @@ async function getSubCategories(req, res) {
       break;
   }
 
-  res.render("categorylistpage", {
-    items: items,
-    name: name,
-    category: req.params.category,
-  });
+  if (items) {
+    res.render("categorylistpage", {
+      items: items,
+      name: name,
+      category: req.params.category,
+    });
+  } else {
+    res.render("404");
+  }
 }
 
 async function getItems(req, res) {
   let items = null;
+  let name = null;
 
   switch (req.params.category) {
     case "size":
       items = await db.getItemsBySize(req.params.subcategory);
+      name = "Size";
       break;
     case "brand":
       items = await db.getItemsByBrand(req.params.subcategory);
+      name = "Brand";
       break;
     case "switch":
       items = await db.getItemsBySwitch(req.params.subcategory);
+      name = "Switch";
       break;
     case "profile":
       items = await db.getItemsByProfile(req.params.subcategory);
+      name = "Profile";
       break;
     case "case-material":
       items = await db.getItemsByCaseMaterial(req.params.subcategory);
+      name = "Case Material";
       break;
   }
 
-  res.render("index", { items: items });
+  res.render("subcategorypage", {
+    items: items,
+    name: name,
+    subcategory: req.params.subcategory,
+  });
 }
 
 module.exports = { getSubCategories, getItems };
